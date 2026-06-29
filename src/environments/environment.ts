f1d2cf857af87a7c5dc407fc1e2bb1c78a8c580e
Andres Leon Fernandez <andres05leonfernandez@gmail.com>
2026-06-28 21:26:47 -0500

    Update API URL for development environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // Entorno de DESARROLLO
 export const environment = {
   production: false,
-  apiUrl: 'https://bfbmhj35-8080.brs.devtunnels.ms/',
+  apiUrl: 'https://sublime-beauty-production-652d.up.railway.app/api',
 };
 
 //https://bfbmhj35-8080.brs.devtunnels.ms/
